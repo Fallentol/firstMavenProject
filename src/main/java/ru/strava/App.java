16cf602692b5a50dde385150930915c908a9519b
Fallentol <Lugvas1@gmail.com>
2018-08-06 00:23:10 +0300

    Update 7

--- src/main/java/ru/strava/App.java
+++ src/main/java/ru/strava/App.java
@@ -2,6 +2,6 @@ package ru.strava;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world  Last test");
     }
 }

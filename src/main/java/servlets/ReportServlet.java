ed0ae28980c8b938e59917169ee674475a91bf88
Fallentol <Lugvas1@gmail.com>
2018-08-06 23:21:00 +0300

    Update 15

--- src/main/java/servlets/ReportServlet.java
+++ src/main/java/servlets/ReportServlet.java
@@ -12,6 +12,7 @@ public class ReportServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         super.doPost(request, response);
+        System.out.println("ReportServlet");
     }
 
     @Override

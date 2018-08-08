831395b688e82ec2000f9b43325124c89553ba40
Fallentol <Lugvas1@gmail.com>
2018-08-08 14:37:09 +0300

    Update 19

---
+++ src/main/java/servlets/IndexServlet.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class IndexServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doPost(request, response);
+        System.out.println("ReportServlet");
+        System.out.println("ReportServlet3242");
+        System.out.println("ReportServlet3242");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        String message = "Report data";
+        request.setAttribute("message", message);
+        request.getRequestDispatcher("WEB-INF/layout/indexJSP.jsp").forward(request, response);
+    }
+}
